696c68d53676f8e255347a30040ee90a8af31a02
Raul <jimenezraul1981@gmail.com>
2022-01-03 01:59:48 -0500

    add function to prompt user for character types to include in password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,64 @@
 // Assignment code here
-var generatePassword = function () {
-  let password = "";
-  let passwordLength = "";
-  const characters = {
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numeric: "0123456789",
-    special: "!@#$%^&*()_+~`|}{[]:;?><,./-="
-  };
+let password = "";
+let passwordLength = "";
+const characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numeric: "0123456789",
+  special: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
+};
+var characterTypes = {};
 
-  // Prompt user for password length from 8 to 128 characters
-  var passwordLengthPrompt = function () {
-    let passLength = prompt(
-      "How many characters would you like your password to be? from 8 to 128"
+// Prompt user for password length from 8 to 128 characters
+var passwordLengthPrompt = function () {
+  let passLength = prompt(
+    "How many characters would you like your password to be? from 8 to 128"
+  );
+  if (
+    passLength.match(/[^0-9]/) ||
+    parseInt(passLength) < 8 ||
+    parseInt(passLength) > 128
+  ) {
+    alert("Please choose a number between 8 and 128");
+    passwordLengthPrompt();
+  } else {
+    passwordLength = parseInt(passLength);
+  }
+};
+
+// Prompt user for character types to include in password
+var characterTypesPrompt = function () {
+  function confirm(type) {
+    return window.confirm(
+      `Would you like to include ${type}? Select 'OK' for yes or 'Cancel' for no.`
     );
-    if (passLength.match(/[^0-9]/) || parseInt(passLength) < 8 || parseInt(passLength) > 128) {
-      alert("Please choose a number between 8 and 128");
-      passwordLengthPrompt();
-    } else {
-      passwordLength = parseInt(passLength);
-    }
-  };
-  // Password length is set to the variable passwordLength
-  passwordLengthPrompt();
+  }
+  // All character that will be prompted
+  characterTypes.uppercase = confirm("uppercase letters");
+  characterTypes.lowercase = confirm("lowercase letters");
+  characterTypes.numbers = confirm("numbers");
+  characterTypes.specials = confirm("special characters");
 
-  console.log(passwordLength);
+  let valid = false;
+  //Validate user input
+  for (let key in characterTypes) {
+    if (characterTypes[key] === true) {
+      valid = true;
+    }
+  }
+  //If is valid, will console log the characterTypes object else will prompt user to enter again
+  if (valid) {
+    console.log(characterTypes);
+  } else {
+    alert("Please select at least one character type");
+    characterTypesPrompt();
+  }
+};
+
+var generatePassword = function () {
+  passwordLengthPrompt();
+  characterTypesPrompt();
+  console.log(characterTypes);
 };
 
 // Get references to the #generate element
@@ -36,7 +70,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
